f0fadb55c029745df27fa912f2669d92abbf4a9a
Davi Nakano <davinotdavid@gmail.com>
2022-10-31 16:28:24 -0700

    chore: frontend initial commit from create-next-app --ts + cleanup

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Dwelling — Card Balance Checker</title>
+        <meta name="description" content="Part of the technical test for Senior Frontend Developer at Dwelling.co" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      </main>
+
+    </div>
+  )
+}
+
+export default Home
